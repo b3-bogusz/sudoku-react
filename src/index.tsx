0a446ad7422a7478d26744a6c786ecf15600ebb0
Patryk Bogusz <patryk@MacBook-Air-Patryk.local>
2022-01-31 18:24:24 +0100

    add Content, Title, Card styled components

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
 
+import { Content, Title, Card } from "./components";
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles, theme } from "./styles";
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <div>Hello World</div>
+        <Content>
+            <Title>
+                Sudoku
+            </Title>
+            <Card>
+                This is Sudoku App
+            </Card>
+        </Content>
     </ThemeProvider>,
   document.getElementById('root')
 );
