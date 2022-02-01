a6de2c54e55e8438ceaf01ea3077e4d090bd0278
Patryk Bogusz <b3.bogusz@gmail.com>
2022-02-01 19:46:45 +0100

    Add Block component, add styles and borders

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,17 +1,19 @@
 import React, { FC, Children } from 'react'
 
-const Grid: FC = () => {
+import Block from "./block";
+import { Container, Row } from "./styles";
 
+const Grid: FC = () => {
     return(
-        <div data-cy="grid-container">
+        <Container data-cy="grid-container">
             {Children.toArray([...Array(9)].map((_, rowIndex) => (
-            <div data-cy="grid-row-container">
-                {Children.toArray([...Array(9).map((_, colIndex) =>
-                    <div data-cy="block">{colIndex}</div>
-                )])}
-            </div>
+            <Row data-cy="grid-row-container">
+                {Children.toArray([...Array(9)].map((_, colIndex) => (
+                    <Block colIndex={colIndex} rowIndex={rowIndex}/>
+                )))}
+            </Row>
             )))}
-        </div>
+        </Container>
     )
 }
 
