a91d24b395fedcbbe15eb5dcac3aa332d0dacd73
Patryk Bogusz <b3.bogusz@gmail.com>
2022-02-04 23:10:51 +0100

    add test for the fillGrid function and update the Grid component

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,9 +1,26 @@
 import React, { FC, Children } from 'react'
 
+import { fillGrid } from 'utils'
+import { GRID } from "typings";
 import Block from "./block";
 import { Container, Row } from "./styles";
 
 const Grid: FC = () => {
+
+    const grid: GRID = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    fillGrid(grid);
+    console.log('grid', grid);
+
     return(
         <Container data-cy="grid-container">
             {Children.toArray([...Array(9)].map((_, rowIndex) => (
--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,19 +1,5 @@
 import { GRID, NUMBERS } from 'typings'
 import { identifySquare,isInRow, isInCol, isInSquare, checkGrid, shuffle} from 'utils';
-import identifyWorkingSquare from "../identify-square";
-
-
-const gridExample = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
 
 const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -30,7 +16,6 @@ function fillGrid(grid: GRID) {
 
     for (let i = 0; i < 81; i++) {
         row = Math.floor(i / 9);
-        console.log('row', row)
         col = i % 9
 
         if (grid[row][col] === 0) {
@@ -38,24 +23,21 @@ function fillGrid(grid: GRID) {
             //do stuff
             for (let value of numbers) {
                 //is it not in grid row?
-                if (!isInRow({ grid, row, value })) {
+                if (!isInRow({ grid, row, value }))
                     // is it not in grid col?
-
                     if (!isInCol({ col, grid, value })) {
                         //is it not on the grid square?
                         const square = identifySquare({ col, grid, row })
-                        if (!isInSquare({ square, value}))
-                        // ....... if this is the case
-
-                        grid[row][col] = value
-                        //check grid if it is full, if yes, stop and return
-                        if (checkGrid(grid)) return true
-                        //otherwise we run fillGrid(grid)
-                        else if (fillGrid(grid)) return true
+                        if (!isInSquare({ square, value})) {
+                            // ....... if this is the case
+                            grid[row][col] = value
+                            //check grid if it is full, if yes, stop and return
+                            if (checkGrid(grid)) return true
+                            //otherwise we run fillGrid(grid)
+                            else if (fillGrid(grid)) return true
+                        }
                     }
-                }
             }
-            // return
             break;
         }
     }
